50135c25940e3fe30fe808cc1779590b7f4f5551
oz115 <yjhg1105@naver.com>
2023-04-10 10:11:30 +0900

    다른연산도 가능하게 하기

--- calc.py
+++ calc.py
@@ -2,13 +2,18 @@
 # return sum
 def plus(x, y):
     return x+y
-
+def minus(x, y):
+    return x+y
+def multi(x, y):
+    return x+y
+def divi(x, y):
+    return x+y
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
     while check >= 1:        
-        print("0: exit, 1: plus")
+        print("0: exit, 1: plus, 2: minus, 3: multiplication, 4:division")
         check = int(input())
         if check == 1:
             print("First Number")
@@ -16,7 +21,29 @@ def main():
             print("Second Number")
             y = int(input())
             print("answer : ", plus(x,y))
-        elif check > 1:
+
+        elif check == 2:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", minus(x,y))
+
+        elif check == 3:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", multi(x,y))
+
+        elif check == 4:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", divi(x,y))
+       
+        elif check > 4:
             print("Unsupported")
         else:
             print("Thank you")
